a7fded8fd9ad70903551bfc5325986382b7378cd
Niklas P <contact@niklasplessing.net>
2024-05-23 13:03:01 +0200

    wip

---
+++ dist/types/index.js
@@ -0,0 +1,4 @@
+export { a as AssetType, b as SubstrateExplorer, c as SubstrateWalletPlatform, d as UseInkathonErrorCode } from '../chunk-5KPKWO5O.js';
+import '../chunk-3AQ3E45C.js';
+//# sourceMappingURL=out.js.map
+//# sourceMappingURL=index.js.map
---
+++ dist/wallets.js
@@ -0,0 +1,6 @@
+export { e as alephzeroSigner, h as allSubstrateWallets, k as enableWallet, i as getSubstrateWallet, j as isWalletInstalled, f as nightly, g as nightlyConnect, d as nova, a as polkadotjs, b as subwallet, c as talisman } from './chunk-E3TXESVT.js';
+import './chunk-R4LN4DEY.js';
+import './chunk-5KPKWO5O.js';
+import './chunk-3AQ3E45C.js';
+//# sourceMappingURL=out.js.map
+//# sourceMappingURL=wallets.js.map
